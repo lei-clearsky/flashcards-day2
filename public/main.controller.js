bd85c81d24786c53cbb5dd1458e425a6d4f06104
Old Gobbo <ramnikarora@gmail.com>
2015-03-31 21:43:28 -0400

    Added Statistics

--- public/main.controller.js
+++ public/main.controller.js
@@ -1,10 +1,69 @@
-app.controller('MainController', function ($scope, whateverName) {
-	$scope.flashCards = whateverName;
+var app = angular.module('flashCards', []);
 
-	$scope.answerQuestion = function (answer, flashCard) {
-		if (!flashCard.answered) {
-			flashCard.answered = true;
-			flashCard.answeredCorrectly = answer.correct;
+app.factory('FlashCardFactory', function($http) {
+	return {
+		getFlashCards: function(category) {
+			var url = '/cards';
+			if (category){
+				url += '?category=' + category;
+			}
+
+			return $http.get(url)
+				.then(function(res) {
+					return res.data;
+			})
 		}
 	}
+});
+
+app.controller('MainController', function ($scope, FlashCardFactory, ScoreFactory) {
+	
+	$scope.categories = [
+		'MongoDB',
+		'Express',
+		'Angular',
+		'Node'
+	];
+
+	$scope.currentCategory = "";
+	
+	$scope.getCategoryCards = function (category) {
+		$scope.currentCategory = category;
+		FlashCardFactory.getFlashCards(category).then(function(data) {
+			$scope.flashCards = data;
+		})
+	}
+	
+	FlashCardFactory.getFlashCards().then(function(data) {
+		$scope.flashCards = data;
+	})
+
+	$scope.answerQuestion = function(answer, flashCard) {
+		ScoreFactory.update(answer.correct);
+	}
+});
+
+app.factory('ScoreFactory', function() {
+	var get = {correct: 0, incorrect: 0};
+
+	return {
+		get: get,
+		update: function(boolVar) {
+			(boolVar) ? get.correct++: get.incorrect++;
+		}
+	}
+});
+
+
+//app.factory('ScoreFactory', function () {
+//	return {
+//		correct: 0,
+//		incorrect: 0 
+//	}
+//});
+//
+
+app.controller('StatsController', function($scope, ScoreFactory) {
+	// $scope.scores = ScoreFactory;
+	$scope.scores = ScoreFactory.get;
 });
